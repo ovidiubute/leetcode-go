70f9a80454f4843614f71cdca85e767d05997e34
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-10-12 14:52:09 +0000

    Fix code with gofmt.

--- 0001.two-sum.go
+++ 0001.two-sum.go
@@ -4,22 +4,22 @@ import "fmt"
 
 func twoSum(nums []int, target int) []int {
 	db := map[int]int{}
-	
+
 	for i, n := range nums {
-			db[n] = i
+		db[n] = i
 	}
-	
+
 	for i, n := range nums {
-			j, ok := db[target - n]
-			
-			if (ok && i != j) {
-					return []int{i, j}
-			}
+		j, ok := db[target-n]
+
+		if ok && i != j {
+			return []int{i, j}
+		}
 	}
-	
+
 	return []int{0, 0}
 }
 
 func main() {
-	fmt.Println(twoSum([]int{3,2,4}, 6))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
 }
