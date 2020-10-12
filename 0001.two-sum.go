d9b440cd5826b7aa94dcbb4928899fe4c899978c
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-10-12 14:50:31 +0000

    Add solution to 1.

---
+++ 0001.two-sum.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	db := map[int]int{}
+	
+	for i, n := range nums {
+			db[n] = i
+	}
+	
+	for i, n := range nums {
+			j, ok := db[target - n]
+			
+			if (ok && i != j) {
+					return []int{i, j}
+			}
+	}
+	
+	return []int{0, 0}
+}
+
+func main() {
+	fmt.Println(twoSum([]int{3,2,4}, 6))
+}
